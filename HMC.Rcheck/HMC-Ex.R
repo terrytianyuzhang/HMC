fba46665e74cdf4bd5fa30468dd49b12dac86963
Tianyu Zhang 张天宇 <zty@uw.edu>
2024-03-05 10:52:17 -0500

    Address the errors and warnings given by check-as-cran
    There are still some notes

---
+++ HMC.Rcheck/HMC-Ex.R
@@ -0,0 +1,150 @@
+pkgname <- "HMC"
+source(file.path(R.home("share"), "R", "examples-header.R"))
+options(warn = 1)
+base::assign(".ExTimings", "HMC-Ex.timings", pos = 'CheckExEnv')
+base::cat("name\tuser\tsystem\telapsed\n", file=base::get(".ExTimings", pos = 'CheckExEnv'))
+base::assign(".format_ptime",
+function(x) {
+  if(!is.na(x[4L])) x[1L] <- x[1L] + x[4L]
+  if(!is.na(x[5L])) x[2L] <- x[2L] + x[5L]
+  options(OutDec = '.')
+  format(x[1L:3L], digits = 7L)
+},
+pos = 'CheckExEnv')
+
+### * </HEADER>
+library('HMC')
+
+base::assign(".oldSearch", base::search(), pos = 'CheckExEnv')
+base::assign(".old_wd", base::getwd(), pos = 'CheckExEnv')
+cleanEx()
+nameEx("anchored_lasso_testing")
+### * anchored_lasso_testing
+
+flush(stderr()); flush(stdout())
+
+base::assign(".ptime", proc.time(), pos = "CheckExEnv")
+### Name: anchored_lasso_testing
+### Title: Simple plug-in test for two-sample mean comparison.
+### Aliases: anchored_lasso_testing
+
+### ** Examples
+
+sample_size_1 <- sample_size_2 <- 300
+true_mean_1 <- matrix(c(rep(1, 10), rep(0, 90)), ncol = 1)
+true_mean_2 <- matrix(c(rep(1.5, 10), rep(0, 90)), ncol = 1)
+
+sample_1 <- data.frame(MASS::mvrnorm(sample_size_1,
+                               mu = true_mean_1,
+                               Sigma = diag(1, 100)))
+ sample_2 <- data.frame(MASS::mvrnorm(sample_size_2,
+                               mu = true_mean_2,
+                               Sigma = diag(1, 100)))
+ result <- anchored_lasso_testing(sample_1, sample_2)
+ result$test_statistics
+ ##the test statistic. It should follow normal(0,1) when there is no difference between the groups.
+ summarize_feature_name(result) 
+ #summarize which features contribute to discriminant vectors (i.e. logistic lasso)
+ extract_pc(result) # extract the estimated discriminant coefficients
+ 
+
+
+
+base::assign(".dptime", (proc.time() - get(".ptime", pos = "CheckExEnv")), pos = "CheckExEnv")
+base::cat("anchored_lasso_testing", base::get(".format_ptime", pos = 'CheckExEnv')(get(".dptime", pos = "CheckExEnv")), "\n", file=base::get(".ExTimings", pos = 'CheckExEnv'), append=TRUE, sep="\t")
+cleanEx()
+nameEx("debiased_pc_testing")
+### * debiased_pc_testing
+
+flush(stderr()); flush(stdout())
+
+base::assign(".ptime", proc.time(), pos = "CheckExEnv")
+### Name: debiased_pc_testing
+### Title: Debiased one-step test for two-sample mean comparison. A small
+###   p-value tells us not only there is difference in the mean vectors,
+###   but can also indicates which principle component the difference
+###   aligns with.
+### Aliases: debiased_pc_testing
+
+### ** Examples
+
+sample_size_1 <- sample_size_2 <- 300
+
+true_mean_1 <- matrix(c(rep(1, 10), rep(0, 90)), ncol = 1)
+true_mean_2 <- matrix(c(rep(1.5, 10), rep(0, 90)), ncol = 1)
+pc1 <- c(rep(1, 10), rep(0, 90))
+pc1 <- pc1/norm(pc1, type = '2')
+
+simulation_covariance <- 10 * pc1 %*% t(pc1)
+simulation_covariance <- simulation_covariance + diag(1, 100)
+
+sample_1 <- data.frame(MASS::mvrnorm(sample_size_1,
+                               mu = true_mean_1,
+                               Sigma = simulation_covariance))
+ sample_2 <- data.frame(MASS::mvrnorm(sample_size_2,
+                               mu = true_mean_2,
+                               Sigma = simulation_covariance))
+ result <- debiased_pc_testing(sample_1, sample_2)
+ result$test_statistics
+ ##these are test statistics. Each one of them corresponds to one PC.
+ summarize_pc_name(result, latent_fator_index = 1) #shows which features contribute to PC1
+ extract_pc(result) # extract the estimated leading PCs.
+ 
+ 
+
+
+
+base::assign(".dptime", (proc.time() - get(".ptime", pos = "CheckExEnv")), pos = "CheckExEnv")
+base::cat("debiased_pc_testing", base::get(".format_ptime", pos = 'CheckExEnv')(get(".dptime", pos = "CheckExEnv")), "\n", file=base::get(".ExTimings", pos = 'CheckExEnv'), append=TRUE, sep="\t")
+cleanEx()
+nameEx("simple_pc_testing")
+### * simple_pc_testing
+
+flush(stderr()); flush(stdout())
+
+base::assign(".ptime", proc.time(), pos = "CheckExEnv")
+### Name: simple_pc_testing
+### Title: Simple plug-in test for two-sample mean comparison.
+### Aliases: simple_pc_testing
+
+### ** Examples
+
+sample_size_1 <- sample_size_2 <- 300
+
+true_mean_1 <- matrix(c(rep(1, 10), rep(0, 90)), ncol = 1)
+true_mean_2 <- matrix(c(rep(1.5, 10), rep(0, 90)), ncol = 1)
+pc1 <- c(rep(1, 10), rep(0, 90))
+pc1 <- pc1/norm(pc1, type = '2')
+
+simulation_covariance <- 10 * pc1 %*% t(pc1)
+simulation_covariance <- simulation_covariance + diag(1, 100)
+
+sample_1 <- data.frame(MASS::mvrnorm(sample_size_1,
+                               mu = true_mean_1,
+                               Sigma = simulation_covariance))
+ sample_2 <- data.frame(MASS::mvrnorm(sample_size_2,
+                               mu = true_mean_2,
+                               Sigma = simulation_covariance))
+ result <- simple_pc_testing(sample_1, sample_2)
+ result$test_statistics 
+ ##these are test statistics. Each one of them corresponds to one PC.
+ summarize_pc_name(result, latent_fator_index = 1) #shows which features contribute to PC1
+ extract_pc(result) # extract the estimated leading PCs.
+ 
+
+
+
+base::assign(".dptime", (proc.time() - get(".ptime", pos = "CheckExEnv")), pos = "CheckExEnv")
+base::cat("simple_pc_testing", base::get(".format_ptime", pos = 'CheckExEnv')(get(".dptime", pos = "CheckExEnv")), "\n", file=base::get(".ExTimings", pos = 'CheckExEnv'), append=TRUE, sep="\t")
+### * <FOOTER>
+###
+cleanEx()
+options(digits = 7L)
+base::cat("Time elapsed: ", proc.time() - base::get("ptime", pos = 'CheckExEnv'),"\n")
+grDevices::dev.off()
+###
+### Local variables: ***
+### mode: outline-minor ***
+### outline-regexp: "\\(> \\)?### [*]+" ***
+### End: ***
+quit('no')
